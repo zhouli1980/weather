cb781368e93ded42989fd7525c1ea751e714c07e
zhoul <zhoul@11wlw.com>
2016-04-19 16:18:15 +0800

    123

--- Weather/src/org/zhouli/activity/WeatherActivity.java
+++ Weather/src/org/zhouli/activity/WeatherActivity.java
@@ -51,6 +51,8 @@ import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
 import com.baidu.location.LocationClientOption.LocationMode;
 
+//********************************************************************************************
+
 
 public class WeatherActivity extends Activity {
 	
